72571a2edc4c41380494fcfa69dc796c7d6aa620
MariuszMalankiewicz <m2ario123@wp.pl>
2024-11-04 15:11:07 +0100

    add migration and model for book and client

---
+++ database/migrations/2024_11_04_140055_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->boolean('is_borrowed')->default(false);
+            $table->string('author');
+            $table->year('publication_year');
+            $table->string('publisher');
+            $table->foreignId('client_id')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
