389bdb2ba7d08e00eeee370226f96e5b90133742
MariuszMalankiewicz <m2ario123@wp.pl>
2024-11-04 17:14:07 +0100

    add bookstableseeder

---
+++ database/seeders/BooksTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 60; $i++) {
+            DB::table('books')->insert([
+                'title' => $faker->sentence(3),
+                'author' => $faker->name,
+                'publication_year' => $faker->year,
+                'publisher' => $faker->company,
+                'is_borrowed' => $faker->boolean,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
