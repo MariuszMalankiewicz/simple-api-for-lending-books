b8400621e45480c5a6fad5a23f2a5308389bc394
MariuszMalankiewicz <m2ario123@wp.pl>
2024-11-04 16:12:09 +0100

    add Book Details: Contains information about the book's name, author, year of publication, publisher, loan status, and the person who borrowed the book (if borrowed).

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,19 +8,26 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
     public function index(Request $request)
-{
-    $query = Book::query();
+    {
+        $query = Book::query();
 
-    if ($search = $request->input('search')) {
-        $query->where('title', 'like', "%{$search}%")
-              ->orWhere('author', 'like', "%{$search}%")
-              ->orWhereHas('client', function ($q) use ($search) {
-                  $q->where('first_name', 'like', "%{$search}%")
-                    ->orWhere('last_name', 'like', "%{$search}%");
-              });
+        if ($search = $request->input('search')) {
+            $query->where('title', 'like', "%{$search}%")
+                ->orWhere('author', 'like', "%{$search}%")
+                ->orWhereHas('client', function ($q) use ($search) {
+                    $q->where('first_name', 'like', "%{$search}%")
+                        ->orWhere('last_name', 'like', "%{$search}%");
+                });
+        }
+
+        $books = $query->with('client')->paginate(20);
+
+        return response()->json($books);
     }
-
-    $books = $query->with('client')->paginate(20);
-    return response()->json($books);
-}
+    public function show($id)
+    {
+        $book = Book::with('client')->findOrFail($id);
+        
+        return response()->json($book);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
