60c43d48f61a0e6a9fb185fba5a52aa281b8c2ed
MariuszMalankiewicz <m2ario123@wp.pl>
2024-11-04 16:16:14 +0100

    add Customer List: Contains the first and last name of customers.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ClientController;
 
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/{id}', [BookController::class, 'show']);
+
+Route::get('/clients', [ClientController::class, 'index']);
